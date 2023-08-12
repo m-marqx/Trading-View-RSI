10d69146f0f259196b6a42fb1430b755b9bdc9fe
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-08-12 16:37:00 -0300

    feat: add _rma_python function

--- rsi.py
+++ rsi.py
@@ -39,3 +39,49 @@ def _rma_pandas(
         .mean()
     ).rename("RMA")
 
+def _rma_python(
+    source: pd.Series,
+    length: int
+) -> pd.Series:
+    """
+    Calculate the Relative Moving Average (RMA) of the input time series
+    data using pure python.
+
+    Parameters:
+    -----------
+    source : pandas.Series
+        The time series data to calculate the RMA for.
+    length : int
+        The number of periods to include in the RMA calculation.
+
+    Returns:
+    --------
+    pd.Series
+        The calculated RMA time series data.
+
+    Note:
+    -----
+    The pure python version is the only one with precision in the
+    initial RMA values. However, with the simple RMA version,
+    both pandas and python versions will yield the same precision
+    in initial values.
+    """
+    alpha = 1 / length
+    source_pd = _rma_pandas(source, length)[:length]
+    source_values = source[length:].to_numpy().tolist()
+
+    rma_value = float(source_pd.dropna().iloc[0])
+
+    rma_list = [rma_value] + [
+        rma_value := alpha * source_value + ((1 - alpha) * rma_value)
+        for source_value in source_values
+    ]
+
+    rma_series = pd.Series(
+        rma_list,
+        name="RMA",
+        index=source[length - 1:].index
+    )
+
+    return rma_series
+
